1ac492f8fae3f1fd556676a04fef1bf77c9866d3
Rails Udemy Course <enginbulut@live.com>
2020-01-25 18:59:12 +0300

    add toggle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const increamentCount = () => {
     setCount(prevCount => prevCount + 1);
   };
 
-  return <button onClick={increamentCount}>I was clicked {count} times</button>;
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+  };
+
+  return (
+    <React.Fragment>
+      <h2>Counter</h2>
+      <button onClick={increamentCount}>I was clicked {count} times</button>
+
+      <h2>Toggle Light</h2>
+      <div
+        style={{
+          height: "50px",
+          width: "50px",
+          background: isOn ? "yellow" : "gray"
+        }}
+        onClick={toggleLight}
+      />
+    </React.Fragment>
+  );
 };
 
 export default App;
