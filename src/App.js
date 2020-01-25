d5b91a6d598ec506afa0beb251da373710aaad48
Rails Udemy Course <enginbulut@live.com>
2020-01-25 19:04:59 +0300

    first try of useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    document.title = `You have clicked ${count} times`;
+  });
 
   const increamentCount = () => {
     setCount(prevCount => prevCount + 1);
