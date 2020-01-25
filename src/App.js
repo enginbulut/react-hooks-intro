7e326e35e2d9c863b369c089c5ccc0fe60331034
Rails Udemy Course <enginbulut@live.com>
2020-01-25 19:29:58 +0300

    cleaning up listeners without a supportive API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { useState, useEffect } from "react";
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
   const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  );
 
+  let mounted = true;
   useEffect(
     () => {
       document.title = `You have clicked ${count} times`;
       window.addEventListener("mousemove", handleMouseMove);
       window.addEventListener("online", handleOnline);
       window.addEventListener("offline", handleOffline);
+      navigator.geolocation.getCurrentPosition(handleGeolocation);
+      const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
       return () => {
         window.removeEventListener("mousemove", handleMouseMove);
         window.removeEventListener("online", handleOnline);
         window.removeEventListener("offline", handleOffline);
+        navigator.geolocation.clearWatch(watchId);
+        mounted = false;
       };
     },
     [count]
   );
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
 
   const handleOnline = () => {
     setStatus(true);
@@ -68,6 +92,11 @@ const App = () => {
       <p>
         You are <strong>{status ? "online" : "offline"}</strong>
       </p>
+
+      <h2>Geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Speed is {speed ? speed : "0"}</p>
     </React.Fragment>
   );
 };
